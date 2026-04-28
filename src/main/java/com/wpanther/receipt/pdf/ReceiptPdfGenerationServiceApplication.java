bd944a026843dd55784212e3e5e5877ca309427d
Weerachat Wongsawat <rabbit_roger@yahoo.com>
2026-04-28 09:47:13 +0700

    feat: scaffold receipt-pdf-generation-service with pom.xml, main class, and config

---
+++ src/main/java/com/wpanther/receipt/pdf/ReceiptPdfGenerationServiceApplication.java
@@ -0,0 +1,12 @@
+package com.wpanther.receipt.pdf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReceiptPdfGenerationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReceiptPdfGenerationServiceApplication.class, args);
+    }
+}
